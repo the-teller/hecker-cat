e2990fd7a0c88ebeb2794945896833a80dcff191
the-teller <99125839+the-teller@users.noreply.github.com>
2022-06-23 20:55:30 +0600

    Update config.js

--- config.js
+++ config.js
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
+			name: 'YouTube',
 			icon: 'youtube',
-			link: 'https://odysee.com/',
+			link: 'https://youtube.com',
 		},
 	],
 
